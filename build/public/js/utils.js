884cde924a7294efa7ab27662eb9f3e8bf5e726f
kingchi005 <kingchi005@gmail.com>
2024-01-11 13:07:26 +0100

    copy files

---
+++ build/public/js/utils.js
@@ -0,0 +1,28 @@
+const formatDate = (date, future = false) => {
+	const prefix = future ? "" : " ago";
+	const now = new Date();
+	const diffInMs = future
+		? new Date(date).getTime() - now.getTime()
+		: now.getTime() - new Date(date).getTime();
+
+	const diffInSecs = Math.floor(diffInMs / 1000);
+	const diffInMins = Math.floor(diffInSecs / 60);
+	const diffInHrs = Math.floor(diffInMins / 60);
+	const diffInDays = Math.floor(diffInHrs / 24);
+
+	if (diffInSecs < 4) {
+		return "just now";
+	} else if (diffInSecs < 60) {
+		return diffInSecs + ` secs${prefix}`;
+	} else if (diffInMins < 60) {
+		return diffInMins + ` min${prefix}`;
+	} else if (diffInHrs < 24) {
+		return diffInHrs + ` hrs${prefix}`;
+	} else if (diffInDays === 1) {
+		return "yesterday";
+	} else if (diffInDays < 90) {
+		return diffInDays + ` days${prefix}`;
+	} else {
+		return new Date(date).toLocaleDateString();
+	}
+};
